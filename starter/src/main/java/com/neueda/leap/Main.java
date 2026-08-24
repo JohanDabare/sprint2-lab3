62f2121876df04c58fde39620e95a8f0e2a4edba
JohanDabare <johan@dabare.com>
2026-08-24 19:25:31 +0000

    test: edited line 6 of Main.java to create merge conflict

--- starter/src/main/java/com/neueda/leap/Main.java
+++ starter/src/main/java/com/neueda/leap/Main.java
@@ -3,6 +3,6 @@ package com.neueda.leap;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World! Shoutout Lebron James!");
-        System.out.println("I love Lebron!!!!");
+        System.out.println("I really really love Lebron!!!!");
     }
 }
